f3197e4c92f5f01735d3b3768190102af675921a
Toby Petty <tobypetty1@gmail.com>
2019-03-28 23:50:18 +0000

    Improved vpn-openvpn-status so that it supports multiple vpns
    Also should support vpns instantiated via network-manager and systemd

--- polybar-scripts/vpn-openvpn-status/vpn-openvpn-status.sh
+++ polybar-scripts/vpn-openvpn-status/vpn-openvpn-status.sh
@@ -1,3 +1,4 @@
-#!/bin/sh
-
-printf "VPN: " && (pgrep -a openvpn$ | head -n 1 | awk '{print $NF }' | cut -d '.' -f 1 && echo down) | head -n 1
+#!/bin/env bash
+IFS=$'\n'
+printf "VPNs: " && echo $(for process in $(pgrep -a openvpn$); do echo $process | grep -Po '(?<=--remote\ )(\S*)|(?<=--config\ /etc/openvpn/)([A-Za-z0-9_\-]*)(?=\.conf|\.ovpn)|(?<=--config\ )([A-Za-z0-9_\-]*)(?=\.conf|\.ovpn)'; done)
+unset IFS
