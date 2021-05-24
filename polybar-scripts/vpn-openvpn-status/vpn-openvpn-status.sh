65cbb55f753089baa673c165851a0886c4cfadf2
Toby Petty <tobypetty1@gmail.com>
2021-05-24 15:43:29 +0100

    Fix vpn-openvpn-status where config file is passed as positional arguement

--- polybar-scripts/vpn-openvpn-status/vpn-openvpn-status.sh
+++ polybar-scripts/vpn-openvpn-status/vpn-openvpn-status.sh
@@ -5,7 +5,7 @@ if [[ ! "$1" =~ "-noprefix" ]]; then
     out="VPNs:";
 fi;
 for process in $(pgrep -a 'openvpn$'); do
-    out="$out $(echo \\"$process\\" | grep -Po '(?<=--remote\ )(\S*)|(?<=--config\ )(.*)(?=\.conf|\.ovpn)' | xargs basename)"
+    out="$out $(echo \\"$process\\" | grep -Po '(?<=--remote\ )(\S*)|(?<=--config\ )|(?<=openvpn\ )(.*)(?=\.conf|\.ovpn)' | xargs basename)"
 done
 echo "$out"
 unset IFS
